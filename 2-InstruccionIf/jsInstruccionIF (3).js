150ce5bfe8b3e6dd8b51b018db7c0eb5442bde68
Porrino7 <47321453+Porrino7@users.noreply.github.com>
2019-02-11 12:36:12 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,10 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad
+edad=document.getElementById("edad").value;
+if(edad >= 18) {alert("usted es mayor de edad")}
 
+else{ alert("sos menor de edad")}
 
 }//FIN DE LA FUNCIÓN
