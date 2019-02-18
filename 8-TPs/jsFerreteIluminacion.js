643475df8d5e1cba01c92aaba0318e65a5e27723
Porrino7 <47321453+Porrino7@users.noreply.github.com>
2019-02-18 01:25:43 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -5,10 +5,50 @@ B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuent
 C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
 D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
- ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
+ ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó.
 
  */
 function CalcularPrecio () 
-{
- 	
+{var cantidad;
+    var marca;
+    var descuento;
+    var preciocondescuento;
+marca=document.getElementById("Marca").value;
+cantidad=parseInt(document.getElementById("Cantidad").value);
+if (cantidad >= 6){
+ descuento = 35 * 50 / 100 ;
+preciocondescuento = (cantidad * 35 ) - descuento * cantidad;
+document.getElementById("precioDescuento").value = preciocondescuento.toFixed(2);}
+
+else if(cantidad == 5 && marca == "ArgentinaLuz");{
+    descuento = 35 * 40 / 100;
+    preciocondescuento = ((cantidad * 35) - descuento * cantidad);
+    document.getElementById("precioDescuento").value = preciocondescuento.toFixed(2);}
+if(cantidad == 5 && marca != "ArgentinaLuz)");{
+    descuento = 35 * 30 / 100; 
+    preciocondescuento = ((cantidad * 35) - descuento * cantidad);
+    document.getElementById("precioDescuento").value = preciocondescuento.toFixed(2);}
+    
+if (cantidad == 4 && marca == "ArgentinaLuz" || "FelipeLamparas") { 
+     descuento = 35 * 25 / 100;
+    preciocondescuento = ((cantidad * 35 ) - descuento * cantidad);
+    document.getElementById("precioDescuento").value = preciocondescuento.toFixed(2)
+    
+} else { descuento = 35 * 20 / 100;
+       preciocondescuento = ((cantindad * 35 ) - descuento * cantidad);
+       document.getElementById("precioDescuento").value = preciocondescuento.toFixed(2);
+   }
+
+   
+   
+
+
+    
+
+
+
 }
+    
+
+
+
